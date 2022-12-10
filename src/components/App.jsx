a002a3d6524e1b62810b884f10ecdd4e97ef681d
Frederic C <toonvesper@gmail.com>
2022-12-10 10:44:38 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {    
+      setNotes(prevNotes=>{
+        return [
+          ...prevNotes, note
+        ];
+      });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((notes,index) => {
+        return id !== index;
+      });
+    })
+  }
+  
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note,index) => {
+        return (
+          <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDel={deleteNote}/>
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
